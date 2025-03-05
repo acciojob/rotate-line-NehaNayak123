792e9ac7a40666a97e00fe35245b1c427ad80049
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-03-06 00:12:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let line=document.getElementById("line")
+let angle=0
+document.addEventListener("DoMContentLoaded", render())
+
+function render(){
+    setInterval(()=>{
+        angle+=2
+        line.style.transform=`rotate(${angle}deg)`
+    },20)
+}
